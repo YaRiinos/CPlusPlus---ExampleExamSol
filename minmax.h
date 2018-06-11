ce8a269ae1dd949719702a30482dba2697c3ea87
Yarin Knafo <yarin665@gmail.com>
2018-06-11 20:11:08 +0300

    Update minmax.h

--- minmax.h
+++ minmax.h
@@ -5,11 +5,7 @@
 #ifndef EXAM_MINMAX_H
 #define EXAM_MINMAX_H
 
-#include <stdlib.h>
-#include <iterator>
 #include <iostream>
-#include <algorithm>
-#include <set>
 
 using namespace std;
 
@@ -20,19 +16,23 @@ void  minmax(T startV1, T endV1, S startV2, S endV2){
     int le1=distance(startV1,endV1);
     int le2=distance(startV2,endV2);
 
+    //If they equal
     if (le1 == le2){
+        
         T& it1 = startV1;
         S& it2 = startV2;
 
         while(it1!=endV1){
+            //If the val in it1 bigger swap, else do nothing.
             if (*it1 > *it2)
                 iter_swap(it1,it2);
-
+                
+                // Increase by 1 both of them
                 it1++;it2++;
         }
     }
 
-    else{
+    else{//If they are not equal..
         if (le1>le2)
             cout<<"sequence 1 is longer"<<endl;
         else
